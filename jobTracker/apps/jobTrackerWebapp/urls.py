1356411819f63bc027dda68e72e0e18b6f936fc2
Xingdong Bian <xingdong.bian@earlham.ac.uk>
2017-02-21 15:17:47 +0000

    change job uuid regex to allow better compatibility for future

--- jobTracker/apps/jobTrackerWebapp/urls.py
+++ jobTracker/apps/jobTrackerWebapp/urls.py
@@ -4,7 +4,7 @@ import apps.jobTrackerWebapp.views as views
 
 urlpatterns = [
     url(r'^$', views.rest_index, name='rest_index'),
-    url(r'^get_job/(?P<job_id>[a-f0-8-]+)$', views.mongo_get_job, name="get_job"),
+    url(r'^get_job/(?P<job_id>[A-z0-9-]+)$', views.mongo_get_job, name="get_job"),
     url(r'^get_jobs', views.mongo_get_all_jobs, name="get_all_jobs"),
     url(r'^update_job/(?P<server_id>[A-z0-9-\s]+)$', views.mongo_update_job, name="update_job")
 ]
