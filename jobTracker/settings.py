2036c4c81668952356c6c5ed9671aaa119d172da
Xingdong Bian <xingdong.bian@earlham.ac.uk>
2017-02-24 14:33:40 +0000

    change static url

--- jobTracker/settings.py
+++ jobTracker/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/grassroots-test/job_tracker/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "jobTracker", "static")
 ]
